054673489766400d265b120e8858cef214976092
S M Imran Ahmed <smimranahmed@Ss-MacBook-Pro.local>
2021-11-12 02:23:06 +0600

    image caching done but need to solve initial load

--- app/src/main/java/com/imran/photogallery/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/imran/photogallery/ui/home/viewmodel/HomeViewModel.kt
@@ -1,32 +1,15 @@
 package com.imran.photogallery.ui.home.viewmodel
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.bumptech.glide.Glide
 import com.imran.photogallery.data.model.Photos
-import com.imran.photogallery.data.model.entities.PhotoEntity
 import com.imran.photogallery.data.repository.PhotosRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
-import android.media.MediaScannerConnection
-import android.media.MediaScannerConnection.OnScanCompletedListener
-
-import android.os.Environment
-import androidx.core.content.ContextCompat.startActivity
-import androidx.lifecycle.asLiveData
-import androidx.paging.ExperimentalPagingApi
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.lang.Exception
-import java.net.URL
 
 
 @HiltViewModel
